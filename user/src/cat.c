675d3aa04d2cb2d19c29598e6bb988d66405d7d2
Colten Webb <coltenwebb@gmail.com>
2024-03-28 11:12:53 -0400

    small fixes

--- user/src/cat.c
+++ user/src/cat.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
 char buf[512];
 
@@ -32,7 +33,7 @@ main(int argc, char *argv[])
   }
 
   for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], 0)) < 0){
+    if((fd = open(argv[i], O_RDONLY)) < 0){
       printf(1, "cat: cannot open %s\n", argv[i]);
       exit();
     }
